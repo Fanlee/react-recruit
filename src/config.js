fda0fd39ecdc6dfece016835fcbc67a0ee2ee5e8
lihuan <qdcnlh@163.com>
2018-12-07 09:22:15 +0800

    新增注册登陆页面测试

--- src/config.js
+++ src/config.js
@@ -3,8 +3,10 @@ import {Toast} from 'antd-mobile'
 
 axios.interceptors.request.use(config => {
   Toast('正在加载', 0)
+  return config
 })
 
 axios.interceptors.response.use(config => {
   Toast.hide()
+  return config
 })
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import ReactDom from 'react-dom'
-import {createStore, applyMiddleware, compose} from 'redux'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+
+import Login from './container/login/login'
+import Register from './container/register/register'
 
 import reducers from './reducer'
 import './config'
 
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
-  window.devToolsExtension ? window.devToolsExtension : f=>f
+  window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
 
-ReactDom.render(
+ReactDOM.render(
   (<Provider store={store}>
-    <BrowserRouter></BrowserRouter>
-  </Provider>)
-)
+    <BrowserRouter>
+      <div>
+        <Route path='/login' component={Login}></Route>
+        <Route path='/register' component={Register}></Route>
+      </div>
+    </BrowserRouter>
+  </Provider>), document.getElementById('root'))
